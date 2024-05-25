8358f488144af3894e9ae084272f404e3de9412f
hobby345 <168425197+hobby345@users.noreply.github.com>
2024-05-25 12:53:54 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,11 +3,36 @@
 //DATA TYPE IN JAVA 
 // 1 PRIMITIVE TYPE (predefined type it get store as a byte-value)
 // 2 OBJECT/REFRENCE TYPE(user defined type and when called it is being refrenced)
-
 public class Main {
-    
     public static void main(String[] args) {
-        CMP202 cmp=new CMP202();
+        CMP202 cmp = new CMP202();
         cmp.printuserDetails();
+
+        Person person = new Person();
+        person.setName("David Smith");
+        person.setPassword("1234567");
+        System.out.println("Person Name: " + person.getName());
+        System.out.println("Person Password: " + person.getPass());
+
+        Person student = new Person("Bingham", "1234567", "Joe", "BHU/04/05/0056", 0);
+        System.out.println("Student School Name: " + student.getSchoolName());
+        System.out.println("Student Name: " + student.getName());
+        System.out.println("Student Password: " + student.getPass());
+        System.out.println("Student Matric No: " + student.getMatricNo());
+
+        Person staff = new Person("Bingham", "654321", "Abu", "", 667);
+        System.out.println("Staff School Name: " + staff.getSchoolName());
+        System.out.println("Staff Name: " + staff.getName());
+        System.out.println("Staff Password: " + staff.getPass());
+        System.out.println("Staff No: " + staff.getStaffNo());
+
+
+        Physics physics = new Physics();
+        float velocity1 = physics.calculateVelocity(100, 5);
+        System.out.println("Velocity (distance/time): " + velocity1);
+
+        float velocity2 = physics.calculateVelocity(20, 3, 4);
+        System.out.println("Velocity (initial velocity + acceleration * time): " + velocity2);
     }
 }
+
